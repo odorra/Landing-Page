e92c57ac2f8a12f5e54ca9b0f236f7ff08d13108
odorra <72552566+odorra@users.noreply.github.com>
2020-12-21 19:49:38 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -41,37 +41,130 @@ function boundary2() {
     }
   );
 }
- // get the ul element using HTML DOM
-const myul = document.getElementById("navbar__list");
+window.addEventListener('scroll', function (event) {
+    sections.forEach((element)=>{
+        //console.log(element);
+        if (isInViewport(element)) {
+             deactivesec();
+			 deactivelink();
+             element.classList.add('your-active-class');
+             let elm_id = element.id;
+			 //alert(elm_id);
+			 setactivelink(elm_id);
+            //console.log('In viewport!');
+        } else if(window.scrollY==0) {
+             deactivesec();
+			 deactivelink();
+            //console.log('No Change');
+        }
+    }, false);
+});	
+  const projectData = {
+    
+    topic_counts: 0,
+    topic_titles: [],
+    section_id: [],
+    ulid: "",
+    navContainers: [],
+    navClass: [],
+    mylinks: [],
+    create_nav: function() {
+       let x = 1;
+       const fragment = document.createDocumentFragment();
+       for (var i=0; i < allSections.length; i++) {
+         let newli = document.createElement('li');
+         let newlink = document.createElement('a');
+         let title = projectData.topic_titles[i];
+         let newtext = document.createTextNode(title);
+         
+         newli.class = "section";
+         newli.id = "link" + x;
+		 
+		 let att = document.createAttribute("href");
+		 att.value = "#section" + x;
+		 x += 1;
+         newlink.className = "navlink";
+		 newlink.setAttributeNode(att);
+         //newlink.href = "#" + projectData.section_id[i];
+          //projectData.mylinks.push(newli.id);
+          
+          newlink.appendChild(newtext);
+          newli.appendChild(newlink);
+          fragment.appendChild(newli);
+       }
+       x = 0;
+       myul.appendChild(fragment);
+       
+    }
+    
+  
+  };
+  
+
+
+
+  // get all divs 
+  const allSections = document.querySelectorAll(".landing__container");
+  
+  // loop using for of loop then get section firstElementChild.title
+  for (sec of allSections) {
+  
+  // get the firstElementChild Value which H2 or section title to store it
+  const section_title = sec.firstElementChild.textContent;
+  
+  
+  // get section title h2 
+  projectData.topic_titles.push(section_title);
+  
+  // get all section elements
+  const section_parent = sec.parentElement;
+  
+  // get sections id 
+  const section_id = section_parent.id;
+  
+  // push sections id to nav
+  projectData.section_id.push(section_id);
+  
+  
+  // test step
+  //alert(projectData.section_id);
+  }
+  
+  // get topic_counts or sections count
+  projectData.topic_counts = projectData.topic_titles;
+  
+  
+  
+  
+  /* ---------------------##############-------------------------------------------*/
+                            // Nav Menu
+  /* ---------------------##############-------------------------------------------*/
+  
+  // get the ul element using HTML DOM
+  const myul = document.getElementById("navbar__list");
   
   // get the ul element
-projectData['ul'] = myul;
+  projectData['ul'] = myul;
   
   
   // get the ul id
-projectData['ulid'] = projectData.ul.id;
-alert(projectData.ulid);
+  projectData['ulid'] = projectData.ul.id;
+  //alert(projectData.ulid);
   
   
   // feature li 
-let allli = document.querySelectorAll(".section");
-alert(allli);
+  let allli = document.querySelectorAll(".section");
+  //alert(allli);
   
   
   // get the nav container
-let mynav = myul.parentElement;
+  let mynav = myul.parentElement;
   
-let mynav_class = myul.parentElement;
+  let mynav_class = myul.parentElement;
   
   // get nav class name
-alert(mynav.className);
-function openNav() {
-  document.getElementsByClass("navbar__menu").style.width = "250px";
-}
-
-/* Set the width of the side navigation to 0 */
-function closeNav() {
-  document.getElementsByClass("navbar__menu").style.width = "0";
-}
+  //alert(mynav.className);
   
-projectData.create_nav();
+  
+  projectData.create_nav();
+  }
